3ae88f4c63b01696e0e42699e9a74980b5fb9b44
ekZeR <cod-ekz@yandex.ru>
2023-09-04 15:16:30 +0300

    third

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -7,19 +7,15 @@ function solveEquation(a, b, c) {
   arr = [-b / (2 * a)];
   }
   else (discriminant > 0) ;{
-    arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)]
+    arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
   }
   return arr;
 }
 
-function solveEquation();
-
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let monthlypercent = (percent * 0.01) / 12;
+  let monthlypercent = percent * 0.01;
   let debt = amount - contribution;
   let monthlyDebt = debt * (monthlypercent + (monthlypercent / ((Math.pow(1 + monthlypercent, countMonths)) - 1)));
-  let result = monthlyDebt * countMonths + contribution;
-  return result.toFixed(2);
+  let result = (monthlyDebt * countMonths + contribution).toFixed(2);
+  return Number(result);
 }
-
-function calculateTotalMortgage()
