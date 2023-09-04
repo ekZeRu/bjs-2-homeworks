d5b74efda27ccdcea541c2b18bd86673f5c23397
ekZeR <cod-ekz@yandex.ru>
2023-09-04 15:30:11 +0300

    six

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -7,7 +7,7 @@ function solveEquation(a, b, c) {
   arr = [-b / (2 * a)];
   }
   else if (discriminant > 0) {
-    arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+    arr = [(-b + Math.sqrt(discriminant)) / (2 * a), (-b - Math.sqrt(discriminant)) / (2 * a)];
   }
   return arr;
 }
