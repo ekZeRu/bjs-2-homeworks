20c77f7f75a19923912aac055cd2aa237c40cfb0
ekZeR <cod-ekz@yandex.ru>
2023-09-04 15:10:22 +0300

    second

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,10 +4,10 @@ function solveEquation(a, b, c) {
   let arr = [];
   let discriminant = (Math.pow(b, 2) - 4 * a * c);
   if (discriminant === 0) {
-  arr.push(-b / (2 * a));
+  arr = [-b / (2 * a)];
   }
   else (discriminant > 0) ;{
-    arr.push((-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a))
+    arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)]
   }
   return arr;
 }
